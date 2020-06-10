b8593d9d132df6e4b98d28d809e8e2f81a37890c
siyka-marinova <siyka.marinova@progress.com>
2020-06-10 13:20:29 +0300

    First commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,11 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, by, element, protractor } from 'protractor';
+
+const gridToolbarDropdownSpanSelector = by.css('kendo-grid-toolbar.k-grid-toolbar kendo-dropdownlist>span');
+const selectCell = async(rowIndex, columnIndex) => {
+  await element(by.css(`td[ng-reflect-row-index="${rowIndex}"][ng-reflect-column-index="${columnIndex}"] kendo-dropdownlist>span`))
+    .click();
+};
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,9 +14,27 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
+  it('should display welcome message', async () => {
+    await page.navigateTo();
     expect(page.getTitleText()).toEqual('grid-with-dropdowns-test app is running!');
+  });
+
+  it('should select an item from the dropdown in the grid toolbar', async () => {
+    await page.navigateTo();
+    await element(gridToolbarDropdownSpanSelector).sendKeys('m');
+    expect(await element(gridToolbarDropdownSpanSelector).getText()).toEqual('Medium');
+  });
+
+  fit('should select an item from the dropdown in the grid cells', async () => {
+    await page.navigateTo();
+    await selectCell(1, 1);
+    await browser
+        .actions()
+        .sendKeys('m')
+        .sendKeys(protractor.Key.ENTER)
+        .perform();
+    const activeElement = await (await browser.driver.switchTo().activeElement());
+    expect(await activeElement.getText()).toEqual('Medium');
   });
 
   afterEach(async () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
+import { sampleProducts } from './products';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  template: `
+    <kendo-grid [data]="gridData">
+      <ng-template kendoGridToolbarTemplate>
+        <kendo-dropdownlist [(value)]="value" [data]="listItems">
+        </kendo-dropdownlist>
+      </ng-template>
+      <kendo-grid-column field="ProductName">
+        <ng-template
+          kendoGridHeaderTemplate
+          let-column
+          let-columnIndex="columnIndex"
+        >
+          {{ column.field }}({{ columnIndex }})
+        </ng-template>
+      </kendo-grid-column>
+      <kendo-grid-column field="size">
+        <ng-template kendoGridCellTemplate let-dataItem>
+          <kendo-dropdownlist [(value)]="dataItem.size" [data]="listItems">
+          </kendo-dropdownlist>
+        </ng-template>
+      </kendo-grid-column>
+    </kendo-grid>
+  `
 })
 export class AppComponent {
-  title = 'grid-with-dropdowns-test';
+  public gridData: any[] = sampleProducts;
+  public value = "Small";
+  public listItems: Array<string> = [
+    "X-Small",
+    "Small",
+    "Medium",
+    "Large",
+    "X-Large",
+    "2X-Large"
+  ];
+  public title = 'Grid with dropdowns';
 }
