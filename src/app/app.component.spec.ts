c4a5d69cd4823807f37287c5a14d8d83b5177d0a
siyka-marinova <siyka.marinova@progress.com>
2020-06-10 16:11:56 +0300

    test: add unit tests for the dropdown inside the grid

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,87 @@
-import { TestBed, async } from '@angular/core/testing';
+import { GridModule } from '@progress/kendo-angular-grid';
+import {
+  async,
+  TestBed,
+  tick,
+  fakeAsync,
+} from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+import { DropDownListModule, DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { POPUP_CONTAINER } from '@progress/kendo-angular-popup';
+import { ElementRef } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        DropDownListModule,
+        NoopAnimationsModule,
+        GridModule,
+        DropDownsModule
+      ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        {
+          provide: POPUP_CONTAINER,
+          useFactory: () => {
+            return { nativeElement: document.body } as ElementRef;
+          }
+        }
+      ]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  afterEach(() => {
+    Array.from(document.body.querySelectorAll('kendo-popup'))
+        .map(node => document.body.removeChild(node));
   });
 
-  it(`should have as title 'grid-with-dropdowns-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('grid-with-dropdowns-test');
-  });
+  const togglePopupFactory = selector => (fixture: any, toggle: boolean = true) => {
+      const component: any = fixture.debugElement.query(By.css(selector)).componentInstance;
+      component.toggle(toggle);
+      fixture.detectChanges();
+      tick();
+      fixture.detectChanges();
+      tick();
+  };
+  const togglePopup = togglePopupFactory('kendo-dropdownlist');
+  const getPopup = (fixture: any): any => {
+      const popup: any = fixture.debugElement.query(By.css('kendo-dropdownlist')).componentInstance.popupRef;
+      return popup ? popup.popupElement : null;
+  };
+  const ddlFixture = (fixture: any) => fixture.debugElement.query(By.css('kendo-dropdownlist'));
+  const getComponentInstance = (fixture: any) => ddlFixture(fixture).componentInstance;
+  const getPopupElement: any = (fixture: any) => getComponentInstance(fixture).popupRef.popupElement;
 
-  it('should render title', () => {
+  it('should open a dropdown', fakeAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('grid-with-dropdowns-test app is running!');
+
+    togglePopup(fixture);
+
+    const popup: any = getPopupElement(fixture).children[0];
+    // expect(popup).toHaveCssClass('k-popup');
+    expect(popup).not.toBeNull();
+
+    fixture.destroy();
+  }));
+
+  it('should set dropdown value', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+
+    const dropdownlist: any = fixture.debugElement.query(By.css('kendo-dropdownlist')).componentInstance;
+    dropdownlist.onKeyPress({ charCode: 'm'.charCodeAt(0) });
+
+    fixture.detectChanges();
+
+    expect(dropdownlist.selectionService.selected[0]).toEqual(2);
   });
 });
+
